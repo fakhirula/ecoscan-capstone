d0317427a47319c4de2574fd03e4d2931673d488
fakhirula <fakhirula27@gmail.com>
2023-12-17 22:16:16 +0700

    README.md

--- app/routes/secureRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken } = require('../middleware/authMiddleware');
-
-router.get('/secureEndpoint', authenticateToken, (req, res) => {
-    res.json({ message: 'This is a secure endpoint', user: req.user });
-});
-
-module.exports = router;
