370c92cd89f13ebfed5d3302e5be136073dcb75c
fakhirula <fakhirula27@gmail.com>
2023-12-18 19:03:39 +0700

    fix big image

--- app/controllers/scanController.js
+++ app/controllers/scanController.js
@@ -4,6 +4,7 @@ const Multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 const TeachableMachine = require("@sashido/teachablemachine-node");
+const sharp = require('sharp');
 
 const multer = Multer({
     storage: Multer.MemoryStorage,
@@ -73,72 +74,72 @@ exports.getScansByUser = (req, res) => {
     });
 };
 
-exports.insertAndPredict = [multer.single('attachment'), imgUpload.uploadToGcs, async (req, res) => {
-        const users_id = req.user.id;
-        const fullname = req.user.fullname;
-        const date = new Date();
-        var waste_type = null;
-        const filename = req.file ? req.file.cloudStorageObject : '';
-        var imageUrl = '';
+// exports.insertAndPredict = [multer.single('attachment'), imgUpload.uploadToGcs, async (req, res) => {
+//         const users_id = req.user.id;
+//         const fullname = req.user.fullname;
+//         const date = new Date();
+//         var waste_type = null;
+//         const filename = req.file ? req.file.cloudStorageObject : '';
+//         var imageUrl = '';
 
-        if (!req.file) {
-            res.status(400).send({message: 'No image file was provided. Please upload an image.'});
-            return;
-        }
+//         if (!req.file) {
+//             res.status(400).send({message: 'No image file was provided. Please upload an image.'});
+//             return;
+//         }
 
-        if (req.file && req.file.cloudStoragePublicUrl) {
-            imageUrl = req.file.cloudStoragePublicUrl;
-        }
+//         if (req.file && req.file.cloudStoragePublicUrl) {
+//             imageUrl = req.file.cloudStoragePublicUrl;
+//         }
 
-        // Insert the scan
-        imageModel.insertScan(users_id, date, waste_type, filename, imageUrl, async (err, result) => {
-            if (err) {
-                res.status(500).send({message: 'Oops! Something went wrong on our end. Please try again later.'});
-            } else {
-                const insertedScanId = result.insertId;
+//         // Insert the scan
+//         imageModel.insertScan(users_id, date, waste_type, filename, imageUrl, async (err, result) => {
+//             if (err) {
+//                 res.status(500).send({message: 'Oops! Something went wrong on our end. Please try again later.'});
+//             } else {
+//                 const insertedScanId = result.insertId;
 
-                // Predict the image class
-                try {
-                    const predictions = await model.classify({
-                        imageUrl: imageUrl,
-                    });
+//                 // Predict the image class
+//                 try {
+//                     const predictions = await model.classify({
+//                         imageUrl: imageUrl,
+//                     });
 
-                    if (predictions && predictions.length > 0) {
-                        const predictedClass = predictions[0].class;
+//                     if (predictions && predictions.length > 0) {
+//                         const predictedClass = predictions[0].class;
 
-                        // Update the waste_type in the inserted scan
-                        imageModel.updateWasteType(insertedScanId, predictedClass, (updateErr, updateResult) => {
-                            if (updateErr) {
-                                console.error("Error updating waste type:", updateErr);
-                            }
-                        });
+//                         // Update the waste_type in the inserted scan
+//                         imageModel.updateWasteType(insertedScanId, predictedClass, (updateErr, updateResult) => {
+//                             if (updateErr) {
+//                                 console.error("Error updating waste type:", updateErr);
+//                             }
+//                         });
 
-                        res.send({
-                            success: true,
-                            message: 'Image uploaded and classified successfully.',
-                            result: {
-                                userId: users_id,
-                                fullname: fullname,
-                                date: date,
-                                waste_type: predictedClass,
-                                filename: filename,
-                                url: imageUrl
-                            }
-                        });
-                    } else {
-                        res.json({
-                            success: false,
-                            message: 'No predictions could be made. Please ensure the image is clear and try again.'
-                        });
-                    }
-                } catch (predictionError) {
-                    console.error("Prediction error:", predictionError);
-                    res.status(500).json({message: 'An unexpected error occurred during prediction. Please try again later.'});
-                }
-            }
-        });
-    }
-];
+//                         res.send({
+//                             success: true,
+//                             message: 'Image uploaded and classified successfully.',
+//                             result: {
+//                                 userId: users_id,
+//                                 fullname: fullname,
+//                                 date: date,
+//                                 waste_type: predictedClass,
+//                                 filename: filename,
+//                                 url: imageUrl
+//                             }
+//                         });
+//                     } else {
+//                         res.json({
+//                             success: false,
+//                             message: 'No predictions could be made. Please ensure the image is clear and try again.'
+//                         });
+//                     }
+//                 } catch (predictionError) {
+//                     console.error("Prediction error:", predictionError);
+//                     res.status(500).json({message: 'An unexpected error occurred during prediction. Please try again later.'});
+//                 }
+//             }
+//         });
+//     }
+// ];
 
 exports.deleteScan = (req, res) => {
     const id = req.params.id;
@@ -155,3 +156,87 @@ exports.deleteScan = (req, res) => {
         }
     });
 };
+
+
+
+exports.insertAndPredict = [multer.single('attachment'), async (req, res, next) => {
+    if (!req.file) {
+        res.status(400).send({message: 'No image file was provided. Please upload an image.'});
+        return;
+    }
+
+    // Compress the image
+    try {
+        const compressedImageBuffer = await sharp(req.file.buffer)
+            .resize(800) // Resize to the width of 800px
+            .jpeg({ quality: 80 }) // Convert to jpeg format with 80% quality
+            .toBuffer();
+
+        // Replace the original file buffer with the compressed image buffer
+        req.file.buffer = compressedImageBuffer;
+
+        next(); // Proceed to the next middleware
+    } catch (compressionError) {
+        console.error("Compression error:", compressionError);
+        res.status(500).json({message: 'An unexpected error occurred during image compression. Please try again later.'});
+    }
+}, imgUpload.uploadToGcs, async (req, res) => {
+    const users_id = req.user.id;
+    const fullname = req.user.fullname;
+    const date = new Date();
+    var waste_type = null;
+    const filename = req.file ? req.file.cloudStorageObject : '';
+    var imageUrl = '';
+
+    if (req.file && req.file.cloudStoragePublicUrl) {
+        imageUrl = req.file.cloudStoragePublicUrl;
+    }
+
+    // Insert the scan
+    imageModel.insertScan(users_id, date, waste_type, filename, imageUrl, async (err, result) => {
+        if (err) {
+            res.status(500).send({message: 'Oops! Something went wrong on our end. Please try again later.'});
+        } else {
+            const insertedScanId = result.insertId;
+
+            // Predict the image class
+            try {
+                const predictions = await model.classify({
+                    imageUrl: imageUrl,
+                });
+
+                if (predictions && predictions.length > 0) {
+                    const predictedClass = predictions[0].class;
+
+                    // Update the waste_type in the inserted scan
+                    imageModel.updateWasteType(insertedScanId, predictedClass, (updateErr, updateResult) => {
+                        if (updateErr) {
+                            console.error("Error updating waste type:", updateErr);
+                        }
+                    });
+
+                    res.send({
+                        success: true,
+                        message: 'Image uploaded and classified successfully.',
+                        result: {
+                            userId: users_id,
+                            fullname: fullname,
+                            date: date,
+                            waste_type: predictedClass,
+                            filename: filename,
+                            url: imageUrl
+                        }
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        message: 'No predictions could be made. Please ensure the image is clear and try again.'
+                    });
+                }
+            } catch (predictionError) {
+                console.error("Prediction error:", predictionError);
+                res.status(500).json({message: 'An unexpected error occurred during prediction. Please try again later.'});
+            }
+        }
+    });
+}];
